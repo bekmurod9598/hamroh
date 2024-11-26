17e65bb3b1c4d72ecb9bde3db53bc48d816aeb4a
mirjalolinomjonov <mirjalolinomjonov@gmail.com>
2024-11-26 13:03:30 +0500

    add category to index page and performance improvements.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,37 @@
 "use strict";
 
+/** SLIDER */
+class Slider {
+  constructor(img, alt = "", parentSelector) {
+    this.img = img;
+    this.alt = alt;
+  }
+  render() {
+    const element = document.createElement("div");
+    element.classList.add("swiper-slide"); // Slides
+    element.innerHTML = `
+      <img
+        style="width: 100%; object-fit: cover"
+        src="${this.img}"
+        alt="${this.alt}"
+      />
+    `;
+    return element;
+  }
+}
+
+let fragment = new DocumentFragment();
+const sliders = [1, 2, 3, 4, 5];
+sliders.forEach((item) => {
+  const el = new Slider(
+    `https://picsum.photos/id/${1 + item}/1200/900`,
+    "sasa",
+    ".swiper .swiper-wrapper"
+  );
+  fragment.append(el.render());
+});
+document.querySelector(".swiper-wrapper").append(fragment);
+
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   loop: true, // Loop through slides
@@ -19,3 +51,36 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+/** CATEGORIES */
+class Category {
+  constructor(img, alt = "", title) {
+    this.img = img;
+    this.alt = alt;
+    this.title = title;
+  }
+  render() {
+    const element = document.createElement("div");
+    element.classList.add("categories__card");
+    element.innerHTML = `
+      <figure class="categories__card-img">
+        <img loading="lazy" src="${this.img}" alt="${this.alt}" />
+      </figure>
+      <h3 class="categories__card-title">${this.title}</h3>
+    `;
+    return element;
+  }
+}
+
+let categoryFragment = new DocumentFragment();
+const categories = [1, 2, 3, 4, 5];
+categories.forEach((item) => {
+  const el = new Category(
+    "https://picsum.photos/900/500",
+    "alt",
+    "category title"
+  );
+  categoryFragment.append(el.render());
+});
+
+document.querySelector(".main .categories").append(categoryFragment);
--- js/sales.js
+++ js/sales.js
@@ -4,98 +4,71 @@ const salesCard = document.createElement("div");
 const salesWrap = document.querySelector(".sales-wrapper");
 
 class Sales {
-  constructor(
-    img,
-    alt = "",
-    validityPeriod,
-    title,
-    description,
-    view,
-    parentSelector
-  ) {
+  constructor(img, alt = "", validityPeriod, title, description, view) {
     this.img = img;
     this.alt = alt;
     this.validityPeriod = validityPeriod;
     this.title = title;
     this.description = description;
     this.view = view;
-    this.parent = document.querySelector(parentSelector);
   }
 
   render() {
     const element = document.createElement("article");
 
+    element.classList.add("sales-card");
     element.innerHTML = `
-      <article class="sales-card">
-            <!-- img -->
-            <figure class="sales-card__figure">
-              <img
-                class="sales-card__figure-img"
-                loading="lazy"
-                src="${this.img}"
-                alt="${this.alt}"
-              />
-            </figure>
-            <!-- description -->
-            <div class="sales-card__info">
-              <div>
-                <span class="sales-card__datetime"
-                  >${this.validityPeriod?.from} - ${this.validityPeriod.to}</span
-                >
-                <h3 class="sales-card__title">
-                  ${this.title}
-                </h3>
-                <p class="sales-card__description">
-                  ${this.description}
-                </p>
-              </div>
+      <!-- img -->
+      <figure class="sales-card__figure">
+        <img
+          class="sales-card__figure-img"
+          loading="lazy"
+          src="${this.img}"
+          alt="${this.alt}"
+        />
+      </figure>
+      <!-- description -->
+      <div class="sales-card__info">
+        <div>
+          <span class="sales-card__datetime"
+            >${this.validityPeriod?.from} - ${this.validityPeriod.to}</span
+          >
+          <h3 class="sales-card__title">
+            ${this.title}
+          </h3>
+          <p class="sales-card__description">
+            ${this.description}
+          </p>
+        </div>
 
-              <div class="sales-card__action">
-                <span><i class="fa-regular fa-eye"></i> ${this.view}</span>
-                <a href="./sales-slug.html">
-                  <i class="fa-solid fa-arrow-right"></i>
-                </a>
-              </div>
-            </div>
-          </article>
+        <div class="sales-card__action">
+          <span><i class="fa-regular fa-eye"></i> ${this.view} ta</span>
+          <a href="./sales-slug.html">
+            <i class="fa-solid fa-arrow-right"></i>
+          </a>
+        </div>
+      </div>
     `;
-    this.parent.append(element);
+    return element;
   }
 }
 
-new Sales(
-  "https://picsum.photos/900/800",
-  "image",
-  {
-    from: "20.11.2024",
-    to: "19.12.2024",
-  },
-  "Title",
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  108,
-  '#sales .sales-wrapper'
-).render();
-new Sales(
-  "https://picsum.photos/900/800",
-  "image",
-  {
-    from: "20.11.2024",
-    to: "19.12.2024",
-  },
-  "Title",
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  108,
-  '#sales .sales-wrapper'
-).render();
-new Sales(
-  "https://picsum.photos/900/800",
-  "image",
-  {
-    from: "20.11.2024",
-    to: "19.12.2024",
-  },
-  "Title",
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  108,
-  '#sales .sales-wrapper'
-).render();
+let fragment = new DocumentFragment();
+
+const sales = [1, 2, 3];
+sales.forEach((item) => {
+  const el = new Sales(
+    `https://picsum.photos/id/${452 + item}/900/800`,
+    "image alt",
+    {
+      from: "20.11.2024",
+      to: "19.12.2024",
+    },
+    "Title",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    108 + item
+  );
+  fragment.append(el.render());
+});
+
+document.querySelector("#sales .sales-wrapper").append(fragment);
