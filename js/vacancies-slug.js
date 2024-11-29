6af243cad74d21758e36c614e555055b8116091d
mirjalolinomjonov <mirjalolinomjonov@gmail.com>
2024-11-30 00:06:11 +0500

    create vacancy slug page

---
+++ js/vacancies-slug.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function showModal(isShow) {
+  const modal = document.querySelector(".modal");
+  if (isShow) {
+    modal.style.display = "grid";
+  } else {
+    modal.style.display = "none";
+  }
+}
+
+const sendResume = document.querySelector(".send-resume");
+const modalBg = document.querySelector(".modal .modal__bg");
+const submit = document.querySelector(".form .form__submit");
+
+sendResume.addEventListener("click", () => {
+  showModal(true);
+});
+modalBg.addEventListener("click", () => showModal(false));
+submit.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  // submit logic
+  alert("Habar yuborildi.");
+  showModal(false);
+});
