6232672f31479ea472436b3f4aceedaebc7e4d9e
mirjalolinomjonov <mirjalolinomjonov@gmail.com>
2024-12-16 23:19:23 +0500

    add pagination and filter in stores page

--- js/store.js
+++ js/store.js
@@ -1,13 +1,14 @@
 "use strict";
+import { parametrs } from "../helpers/options.js";
 
 class Store {
-  constructor(img, name, address, workTime, phone, map) {
+  constructor(img, name, address, workTime, phone, location) {
     this.img = img;
     this.name = name;
     this.address = address;
     this.workTime = workTime;
     this.phone = phone;
-    this.map = map;
+    this.location = location;
   }
 
   render() {
@@ -43,10 +44,10 @@ class Store {
             <i class="fa-solid fa-phone-volume"></i>
             <a href="tel:${this.phone}" class="store__title">${this.phone}</a>
           </div>
-          <!-- map -->
+          <!-- location -->
           <div class="store__info">
             <i class="fa-solid fa-route"></i>
-            <a href="${this.map}" class="store__title store__title_link">Yoʻnalishlar uchun xaritaga qarang</a>
+            <a href="${this.location}" target="_blank" class="store__title store__title_link">Yoʻnalishlar uchun xaritaga qarang</a>
           </div>
         </div>
       </div>
@@ -55,19 +56,127 @@ class Store {
   }
 }
 
-let fragment = new DocumentFragment();
+const { regions } = parametrs;
+const stores = [];
 
-const stores = [1, 2, 3];
+/** FILTER FORM */
+const area = document.querySelector(".form #area");
+const city = document.querySelector(".form #city");
+const submit = document.querySelector(".form #submit");
+const areaFragmet = new DocumentFragment();
+const cityFragmet = new DocumentFragment();
+
+regions.forEach((item) => {
+  stores.push(...item.branches);
+  const option = document.createElement("option");
+  option.value = item.id;
+  option.innerText = item.name;
+  areaFragmet.append(option);
+});
+area.append(areaFragmet);
+
 stores.forEach((item) => {
-  const el = new Store(
-    "https://picsum.photos/900/650",
-    "Store name",
-    "Address. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    "08:30-17:00",
-    "+998558014444",
-    "#"
-  );
-  fragment.append(el.render());
+  const option = document.createElement("option");
+  option.value = item.id;
+  option.innerText = item.name;
+  cityFragmet.append(option);
+});
+city.append(cityFragmet);
+
+area.addEventListener("change", (e) => {
+  console.log(e.target.value);
+});
+city.addEventListener("change", (e) => {
+  console.log(e.target.value);
+});
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("submit");
 });
 
-document.querySelector(".main .stores-wrapper").append(fragment);
+/** PAGINATION */
+// Pagination variables
+let currentPage = 1;
+const itemsPerPage = 10;
+
+// Function to calculate and display items for the current page
+function paginate(array, page, itemsPerPage) {
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  return array.slice(startIndex, endIndex);
+}
+let fragment = new DocumentFragment();
+// Render the paginated data
+function renderData(items) {
+  const container = document.querySelector(".main .stores-wrapper");
+  container.innerHTML = ""; // Clear previous data
+  items.forEach((item) => {
+    const el = new Store(
+      "../assets/img/stores/hamroh.webp",
+      item.name,
+      item.address,
+      `${item.workTime.open} - ${item.workTime.close}`,
+      "+998558014444",
+      item.location
+    );
+    fragment.append(el.render());
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+
+  container.append(fragment);
+}
+
+// Create pagination controls
+function renderPaginationControls(totalItems, itemsPerPage) {
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  const paginationContainer = document.getElementById("pagination-controls");
+  paginationContainer.innerHTML = ""; // Clear previous controls
+
+  // Create previous button
+  const prevButton = document.createElement("button");
+  prevButton.textContent = "Previous";
+  prevButton.disabled = currentPage === 1;
+  prevButton.addEventListener("click", () => {
+    if (currentPage > 1) {
+      currentPage--;
+      updatePagination();
+    }
+  });
+  paginationContainer.appendChild(prevButton);
+
+  // Create page number buttons
+  for (let i = 1; i <= totalPages; i++) {
+    const button = document.createElement("button");
+    button.textContent = i;
+    button.className = currentPage === i ? "active" : "";
+    button.addEventListener("click", () => {
+      currentPage = i;
+      updatePagination();
+    });
+    paginationContainer.appendChild(button);
+  }
+
+  // Create next button
+  const nextButton = document.createElement("button");
+  nextButton.textContent = "Next";
+  nextButton.disabled = currentPage === totalPages;
+  nextButton.addEventListener("click", () => {
+    if (currentPage < totalPages) {
+      currentPage++;
+      updatePagination();
+    }
+  });
+  paginationContainer.appendChild(nextButton);
+}
+
+// Update pagination view
+function updatePagination() {
+  const paginatedItems = paginate(stores, currentPage, itemsPerPage);
+  renderData(paginatedItems);
+  renderPaginationControls(stores.length, itemsPerPage);
+}
+
+// Initialize
+document.addEventListener("DOMContentLoaded", () => {
+  updatePagination();
+});
