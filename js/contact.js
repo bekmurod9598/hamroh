8aa61159c8a3160cde42927c765cf455a4094d75
mirjalolinomjonov <mirjalolinomjonov@gmail.com>
2024-12-23 10:43:17 +0500

    fix style contact page

--- js/contact.js
+++ js/contact.js
@@ -6,31 +6,3 @@ submit.addEventListener("click", (e) => {
   e.preventDefault();
   console.log("Xabaringiz yuborildi");
 });
-
-const img = {
-  // slider uchun min size
-  slider: {
-    width: "1280px",
-    height: "960px",
-    count: { min: 1, max: "optional" },
-  },
-
-  // product category
-  category: {
-    width: "250px",
-    height: "140px",
-    count: "nechta kategoriya bo'lsa shuncha rasim va nomlari",
-  },
-
-  aksiyalar: {
-    width: "1280px",
-    height: "480px",
-    count: { min: 1, max: "optional" },
-  },
-  
-  dokonlar: {
-    width: "580px",
-    height: "350px",
-    count: { min: 1, max: "optional" },
-  },
-};
